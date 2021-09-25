ff9b633eb7edf6ff09d91b69babbfd5377ff04a6
Erencan ÇELİK <celik.erenn96@gmail.com>
2021-09-25 16:51:48 +0300

    comment lines edited, function checked

--- src/components/FilesComp.js
+++ src/components/FilesComp.js
@@ -7,17 +7,16 @@ import Input from '@mui/material/Input';
 import SendIcon from '@mui/icons-material/Send';
 
 function FilesComp() {
-  const [fImage, setFimage] = useState(false);
-  const [bImage, setBimage] = useState(false);
+
+  //first input
+  const [firstImage, setFirstImage] = useState(false);
+  //second input
+  const [secondImage, setSecondImage] = useState(false);
+  //dropzone
   const [file, setFile] = useState(false);
 
-  // console.log("first image button: ",fImage);
-  // console.log("second image button: ",bImage);
-  // console.log("files: ",file);
-  // console.log("send: ",file&&bImage&&fImage)
 
-
-//Dropzoneare style
+  //DropZoneArea style
   const useStyles = makeStyles(theme => createStyles({
     previewChip: {
       minWidth: 160,
@@ -25,6 +24,7 @@ function FilesComp() {
     },
   }));
   const classes = useStyles();
+
   return (
     <Box
       sx={{
@@ -35,24 +35,23 @@ function FilesComp() {
         flexDirection: 'column',
         justifyContent: 'space-evenly',
         alignItems: 'center',
-        backgroundImage:'linear-gradient(45deg, rgba(245, 245, 245, 0.7) 30%, rgba(183,177,165,0.5) 30% 60%, rgba(245, 245, 245, 0.7) 60%)'
+        backgroundImage: 'linear-gradient(45deg, rgba(245, 245, 245, 0.7) 30%, rgba(183,177,165,0.5) 30% 60%, rgba(245, 245, 245, 0.7) 60%)'
       }}>
       <label htmlFor="contained-button-file">
-        <p>Please upload the <b>front</b> of your passport.</p><br/>
-        <Input accept="image/*" id="contained-button-file" filesLimit={1} onChange={()=>setFimage(true)} type="file"/>
+        <p>Please upload the <b>front</b> of your passport.</p><br />
+        <Input accept="image/*" id="contained-button-file" filesLimit={1} onChange={() => setFirstImage(true)} type="file" />
       </label>
       <label htmlFor="contained-button-file1">
-      <p>Please upload the <b>back</b> of your passport.</p><br/>
-        <Input accept="image/*" id="contained-button-file1" filesLimit={1} onChange={()=>setBimage(true)} type="file"/>
+        <p>Please upload the <b>back</b> of your passport.</p><br />
+        <Input accept="image/*" id="contained-button-file1" filesLimit={1} onChange={() => setSecondImage(true)} type="file" />
       </label>
-      <p style={{marginBottom:'-40px'}}>Please upload your <b>payslip(s)</b>.</p>
+      <p style={{ marginBottom: '-40px' }}>Please upload your <b>payslip(s)</b>.</p>
       <Box sx={{
         width: '20vw',
         height: '25vh',
         bgcolor: 'white'
       }}>
         <DropzoneArea
-          multiple={false}
           showPreviews={false}
           showPreviewsInDropzone={true}
           useChipsForPreview
@@ -60,19 +59,19 @@ function FilesComp() {
           previewChipProps={{ classes: { root: classes.previewChip } }}
           previewText="Selected files"
           filesLimit={20}
-          onChange={(files)=>files.length === 0 ? setFile(false) : setFile(true)}
+          onChange={(files) => files.length === 0 ? setFile(false) : setFile(true)} //Here we check the length of the array inside the dropzone
         />
       </Box>
-        <Button
-          variant="contained"
-          endIcon={<SendIcon />}
-          disabled={file&&bImage&&fImage ? false : true}
-          sx={{bgcolor: '#F5F5F5', color:'black'}}
-        >
-          Send
-        </Button>
+      <Button
+        variant="contained"
+        endIcon={<SendIcon />}
+        disabled={file && secondImage && firstImage ? false : true} //Here we decide whether the button is active or not
+        sx={{ bgcolor: '#F5F5F5', color: 'black' }}
+      >
+        Send
+      </Button>
     </Box>
   );
 }
 
-export default FilesComp
+export default FilesComp;
